96992712887219a29c56dc306aafcebd8784ffa5
Atym-git <chuvachelaad@gmail.com>
2024-09-25 07:40:26 +0300

    Release

---
+++ Laboratory Layout/Assets/Scripts/MainMenuView.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuView : MonoBehaviour
+{
+
+    [SerializeField] private Button playButton;
+    [SerializeField] private GameObject levelMenu;
+    [SerializeField] private GameObject levelButtonPrefab;
+    [SerializeField] private RectTransform root;
+    [SerializeField] private int levelCount = 0;
+
+    private const int _minstarscount = 0;
+    private const int _maxstarscount = 4;
+
+    void Start()
+    {
+        levelMenu.SetActive(false);
+        playButton.onClick.AddListener(OnplayButtonPressed);
+        playButton.onClick.AddListener(FillLevelMenu);
+
+    }
+
+    private void OnplayButtonPressed()
+    {
+        playButton.gameObject.SetActive(false);
+        ClearLevelMenu();
+        levelMenu.SetActive(true);
+    }
+
+
+    private void OnDestroy()
+    {
+        playButton.onClick.RemoveListener(OnplayButtonPressed);
+    }
+
+    private void FillLevelMenu()
+    { 
+    for(int i = 0; i < levelCount; i++)
+        {
+            GameObject levelbutton = Instantiate(levelButtonPrefab, root);
+            if (levelbutton.TryGetComponent(out LevelButtonView LevelButtonView))
+                
+            {
+
+                LevelButtonView.SetupLevelButton(i + 1, UnityEngine.Random.Range(_minstarscount, _maxstarscount));
+            }
+            //if (levelbutton.TryGetComponent(out Button btn))
+            //{
+            //    btn.onClick.AddListener();
+            //}    
+        }
+    
+    }
+
+    private void ClearLevelMenu()
+    {
+        for(int i = 0; i < root.childCount; i++)
+        {
+            Destroy(root.GetChild(i).gameObject);
+        }
+    }
+
+
+
+}
